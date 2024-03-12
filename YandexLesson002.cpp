872bcc0925f25066830514444dc79254463d89ba
KonstantinSDx <constsa@yandex.ru>
2024-03-12 17:42:32 +0400

    Add YandexLesson002.cpp

---
+++ YandexLesson002.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main()
+{
+	double LenX;
+	std::cin >> LenX;
+	std::cout << LenX/2.54 << "\n";
+	
+	unsigned long long n;
+    std::cin >> n;
+    std::cout << n * (n + 1) / 2 << "\n";
+	return 0;
+}
