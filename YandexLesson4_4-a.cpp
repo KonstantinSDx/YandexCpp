1355727a5c45d806ec27a34cb81f80c296d1f80e
KonstantinSDx <constsa@yandex.ru>
2024-07-13 16:55:04 +0400

    YandexLesson4_4-a.cpp

---
+++ YandexLesson4_4-a.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <cstdlib>
+#include <string>
+
+class Serializer
+{
+	
+	private :
+		std::string JSONstr;
+	public :
+		void BeginArray()
+    	void AddArrayItem(const std::string &)
+		void EndArray()
+	
+};
+
+
+int main()
+{
+	
+	return 0;
+}
